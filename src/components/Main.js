b6ff05d805d0b9c6a35c4340278284244aa1dce0
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-11-14 21:19:17 -0800

    First version of Profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { ShotChart } from './ShotChart'
 import { Profile } from './Profile'
+import nba from 'nba';
 
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: null,
+    }
+    componentDidMount() {
+        nba.stats.playerInfo( { PlayerID: this.state.playerId })
+            .then((response) => {
+                const playerInfo = Object.assign(response.commonPlayerInfo[0], response.playerHeadlineStats[0])
+                console.log(playerInfo)
+                this.setState({ playerInfo })
+            })
+            .catch((e) => console.log(e))
+    }
+
     render() {
         return (
             <div className="main">
-                <Profile />
-                <ShotChart playerId={201939} />
+                <Profile playerInfo={this.state.playerInfo}/>
+                <ShotChart playerId={this.state.playerId} />
             </div>
         )
     }
