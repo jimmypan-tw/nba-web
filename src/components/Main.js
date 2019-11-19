8cdcbe2dd0987f85a7143e1863a48e503c64fa95
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-11-18 22:50:09 -0800

    Implement handleSelectPlayer() and loadPlayerInfo()

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,26 +4,35 @@ import nba from 'nba';
 import { Profile } from './Profile'
 import SearchBar from "./SearchBar"
 import { DataViewContainer } from './DataViewContainer'
+import { DEFAULT_PLAYER_INFO } from "../constants"
 
 export class Main extends React.Component {
     state = {
         playerId: nba.findPlayer('Stephen Curry').playerId,
-        playerInfo: null,
+        playerInfo: DEFAULT_PLAYER_INFO,
     }
     componentDidMount() {
-        nba.stats.playerInfo( { PlayerID: this.state.playerId })
+        this.loadPlayerInfo(this.state.playerInfo.fullName)
+    }
+
+    handleSelectPlayer = (playerName) => {
+        this.loadPlayerInfo(playerName)
+        console.log(playerName)
+    }
+
+    loadPlayerInfo = (playerName) => {
+        nba.stats.playerInfo( { PlayerID: nba.findPlayer(playerName).playerId})
             .then((response) => {
                 const playerInfo = Object.assign(response.commonPlayerInfo[0], response.playerHeadlineStats[0])
                 console.log(playerInfo)
                 this.setState({ playerInfo })
             })
-            .catch((e) => console.log(e))
     }
 
     render() {
         return (
             <div className="main">
-                <SearchBar />
+                <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
                 <div className="player">
                     <Profile playerInfo={this.state.playerInfo}/>
                     <DataViewContainer playerId={this.state.playerId} />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,55 @@
 import React, {Component} from 'react';
-import { Icon, Button, Input, AutoComplete } from 'antd';
+import nba from 'nba';
+import { Icon, Input, AutoComplete } from 'antd';
+import { PROFILE_PIC_URL_PREFIX } from '../constants';
+
+const { Option } = AutoComplete
 
 class SearchBar extends Component {
+    state = {
+        option_dataSource: [],
+    };
+    handleSearch = value => {
+        console.log(value)
+        this.setState({
+            option_dataSource: !value?
+                [] : nba.searchPlayers(value).map(player=> ({
+                    fullName: player.fullName,
+                    playerId: player.playerId,
+                }))
+        })
+    }
+
+    onSelect = (playerName) => {
+        this.props.handleSelectPlayer(playerName)
+    }
+
     render() {
+        const { option_dataSource } = this.state
+        console.log(option_dataSource)
+        const options = option_dataSource.map((player) => (
+            <Option key={player.fullName} value={player.fullName}
+                className="player-option">
+                <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}/>
+                <span className="player-option-label">{player.fullName}</span>
+            </Option>
+        ))
+
+
         return(
                 <AutoComplete
                     className="search-bar"
                     size="large"
                     // style={{ width: '100%' }}
-                    // dataSource={dataSource.map(renderOption)}
-                    // onSelect={onSelect}
-                    // onSearch={this.handleSearch}
+                    dataSource={options}
+                    onSelect={this.onSelect}
+                    onSearch={this.handleSearch}
                     placeholder="Search NBA Player"
                     optionLabelProp="value"
                 >
                     <Input
                         suffix={
-                            <Button
-                                className="search-btn"
-                                style={{ marginRight: -12 }}
-                                size="large"
-                                type="primary"
-                            >
-                                <Icon type="search" />
-                            </Button>
+                                <Icon type="search" className="certain-category-icon"/>
                         }
                     />
                 </AutoComplete>
